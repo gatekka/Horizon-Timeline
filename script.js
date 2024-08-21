e78d7f0235c65986dc18d82f76955c44afed3154
chris <thunderlight465@gmail.com>
2024-08-21 01:55:07 -0700

    Set mouse listening event to document

--- script.js
+++ script.js
@@ -2,15 +2,16 @@ var line = document.getElementById("line");
 var defaultcolor = line.style.background;
 var placepoint = document.getElementById("placepoint");
 var point_data = document.getElementById("point_data");
+var line_container = document.getElementById("line_container");
 
-line.addEventListener('mousemove', getPosition);
+document.addEventListener('mousemove', getPosition);
 
 function getPosition(event) {
     var x = event.pageX; //event.clientX
     var y = event.pageY; //event.clientY
     var positionText= "X: " + x + ", Y: " + y;
     document.getElementById("result").innerText = positionText;
-    placepoint.style.left = x + "px";
+    placepoint.style.left = x + "px"; // move point with mouse
 }
 
 line.onmouseover = function displayPoint() {
