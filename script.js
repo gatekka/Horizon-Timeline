75803514b907600ad1193f1f2d6c26ae2a3aae9c
chris <94351434+gatekka@users.noreply.github.com>
2024-08-21 02:10:58 -0700

    refactoring and added partial submit button functionality

--- script.js
+++ script.js
@@ -3,15 +3,19 @@ var defaultcolor = line.style.background;
 var placepoint = document.getElementById("placepoint");
 var point_data = document.getElementById("point_data");
 var line_container = document.getElementById("line_container");
+var submit_button = document.getElementById("submit_button");
 
 document.addEventListener('mousemove', getPosition);
 
+var mousex;
+var mousey;
+
 function getPosition(event) {
-    var x = event.pageX; //event.clientX
-    var y = event.pageY; //event.clientY
-    var positionText= "X: " + x + ", Y: " + y;
+    mousex = event.pageX;
+    mousey = event.pageY;
+    var positionText= "X: " + mousex + ", Y: " + mousey;
     document.getElementById("result").innerText = positionText;
-    placepoint.style.left = x + "px"; // move point with mouse
+    placepoint.style.left = mousex + "px"; // move point with mouse
 }
 
 line.onmouseover = function displayPoint() {
@@ -21,10 +25,15 @@ line.onmouseover = function displayPoint() {
 
 line.onclick = function placePoint() {
     point_data.style.opacity = "100%";
+    point_data.style.left = mousex + "px";
     console.log("clicked horizon");
 }
 
 line.onmouseleave = function onMouseLeave() {
     line.style.background = defaultcolor;
     placepoint.style.opacity = "0%";
+}
+
+submit_button.onclick = function SubmitData() {
+    point_data.style.opacity = "0%";
 }
