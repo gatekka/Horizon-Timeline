6fd500944c0a8219805e19ac0d6db64920b6749e
chris <94351434+gatekka@users.noreply.github.com>
2024-08-21 03:59:20 -0700

    - Added: focus onto input field. - Added: erase input fields after submitting.

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ line.onmouseover = function displayPoint() {
 line.onclick = function placePoint() {
     point_data.style.opacity = "100%";
     point_data.style.left = mousex + "px";
+    title_data.focus(); // auto focuses to title input field
     console.log("clicked horizon");
 }
 
@@ -36,6 +37,8 @@ line.onmouseleave = function onMouseLeave() {
     placepoint.style.opacity = "0%";
 }
 
+// TODO: Make it so that pressing 'enter' key while focusing on input boxes also executes 'SubmitData' function
+// FIX: Rework uniqueid. Currently restarts back to 1 after importing from localStorage. Make it random x-length string instead of increasing by 1?
 var dataStore = JSON.parse(localStorage.getItem('dataLocalStorage')) || []; // imports from local storage, otherwise creates empty array
 var uniqueid = 1;
 submit_button.onclick = function SubmitData() {
@@ -45,9 +48,15 @@ submit_button.onclick = function SubmitData() {
         description: description_data.value
     }
     dataStore.push(obj); // stores into array
-    uniqueid++;
-    point_data.style.opacity = "0%";
     localStorage.setItem('dataLocalStorage', JSON.stringify(dataStore)); // serializes the array and stores in local storage
-    console.log(JSON.stringify(dataStore)); // for logging purposes
-    // console.log(dataStore); // for logging purposes
+    point_data.style.opacity = "0%";
+    uniqueid++;
+
+    // Clearing input boxes
+    title_data.value = ""; 
+    description_data.value = "";
+
+    // console.log(localStorage); // for logging purposes
+    // console.log(JSON.stringify(dataStore)); // for logging purposes
+    console.log(dataStore); // for logging purposes
 }
