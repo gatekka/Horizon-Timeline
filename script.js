34ac2cf137ac2309800c1fdbdcb5b321230b9005
chris <94351434+gatekka@users.noreply.github.com>
2024-08-23 02:47:35 -0700

    Refactoring

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // TODO: Display all data points on horizon line.
 // TODO: Look into replacing arrays with object constructors? Might be more efficient.
-// TODO: Unfocusing input box should hide point_data
 const line = document.getElementById("line");
 const defaultcolor = line.style.background;
 const placepoint = document.getElementById("placepoint");
@@ -38,6 +37,15 @@ line.onclick = function placePoint() {
     title_data.focus(); // auto focuses to title input field
     console.log("clicked horizon");
 }
+// TODO: Unfocusing input box should hide point_data
+// point_data.addEventListener('focusout', handleFocusOut)
+// line_container.addEventListener('focusout', handleFocusOut)
+// function handleFocusOut(event) {
+//     if (!point_data.contains(event.relatedTarget) && !line_container.contains(event.relatedTarget)) {
+//         cancelData();
+//         console.log('point_data no longer in focus'); // logging
+//     }
+// }
 
 line.onmouseover = function displayPoint() {
     line.style.background = "#8fD362";
@@ -49,6 +57,22 @@ line.onmouseleave = function onMouseLeave() {
         placepoint.classList.add('isHidden');
     }
     line.style.background = defaultcolor;
+}
+
+function cancelData() {
+    inactiveStylingActivate();
+}
+
+function inactiveStylingActivate() {
+    point_data.classList.add('isHidden');
+    placepoint.classList.add('isHidden');
+    placepoint.classList.remove('placepoint-active');
+
+    clickedOnLine = false;
+
+    // Clearing input boxes
+    title_data.value = ""; 
+    description_data.value = "";
 }
 
 // FIX: Rework uniqueid. Currently restarts back to 1 after importing from localStorage. Make it random x-length string instead of increasing by 1?
@@ -64,15 +88,7 @@ function SubmitData() {
     localStorage.setItem('dataLocalStorage', JSON.stringify(dataStore)); // serializes the array and stores in local storage
     uniqueid++;
 
-    point_data.classList.add('isHidden');
-    placepoint.classList.add('isHidden');
-    placepoint.classList.remove('placepoint-active');
-
-    clickedOnLine = false;
-
-    // Clearing input boxes
-    title_data.value = ""; 
-    description_data.value = "";
+    inactiveStylingActivate();
 
     // console.log(localStorage); // for logging purposes
     // console.log(JSON.stringify(dataStore)); // for logging purposes
