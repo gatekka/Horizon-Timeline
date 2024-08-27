62e19d75d19f61ca2ec78e19c449ff6a99a19320
chris <thunderlight465@gmail.com>
2024-08-27 06:42:58 -0700

    - Refactoring - Added placePointOnLine() function - Fixed objPointData.id from restarting back to 1 after importing from localStorage

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 // TODO: Display all data points on horizon line.
 // TODO: Look into replacing arrays with object constructors? Might be more efficient.
 const line = document.getElementById("line");
-const placepoint = document.getElementById("placepoint");
+const hoverPoint = document.getElementById("hoverPoint");
 const point_data = document.getElementById("point_data");
 const line_container = document.getElementById("line_container");
 const submit_button = document.getElementById("submit_button");
 const title_data = document.getElementById("title_data");
 const description_data = document.getElementById("description_data");
 const mouseXYposition = document.getElementById("mouseXYposition");
+const placedPoint = document.getElementById("placedPoint");
 
-var mousex;
-var mousey;
+let mousex;
+let mousey;
 document.addEventListener('mousemove', function getMousePosition(event) {
     mousex = event.pageX;
     mousey = event.pageY;
-    var positionText= "X: " + mousex + ", Y: " + mousey;
+    const positionText= "X: " + mousex + ", Y: " + mousey;
     mouseXYposition.innerText = positionText;
     if (clickedOnLine == false) {
-        placepoint.style.left = mousex + "px"; // move point with mouse
+        hoverPoint.style.left = mousex + "px"; // move point with mouse
     }
 });
 
@@ -33,9 +34,10 @@ function initializePointData() { // Temporarily exposes point_data to store offs
     point_data.classList.add('isHidden');
 }
 
-var clickedOnLine = false;
+ clickedOnLine = false;
 line.onclick = function showPointData() {
     clickedOnLine = true;
+    placePointOnLine();
     point_data.style.transitionBehavior = saveTransitionBehavior;
     if (mousex < point_dataMaxAllowablePosition) {
         point_data.style.left = mousex + "px";
@@ -43,10 +45,22 @@ line.onclick = function showPointData() {
         point_data.style.left = point_dataMaxAllowablePosition + "px";
     }
     point_data.classList.remove('isHidden');
-    placepoint.classList.add('placepoint-active');
-    placepoint.style.left = mousex + 'px';
+    hoverPoint.classList.add('hoverPoint-active');
+    hoverPoint.style.left = mousex + 'px';
     title_data.focus(); // auto focuses to title input field
     console.log("Clicked on timeline."); // Log to console
+}
+
+function placePointOnLine() {
+    placedPoint.classList.remove('isHidden');
+    placedPoint.style.left = mousex + 'px';
+    point_data.classList.add('isHidden');
+    /* placedPoint.addEventListener('mouseover', function() {
+        placedPoint.style.backgroundColor = 'red';
+        point_data.classList.remove('isHidden');
+    }); */
+    line_container.appendChild(placedPoint.cloneNode(true));
+    line_container.appendChild(point_data.cloneNode(true));
 }
 
 document.addEventListener('click', function(event) {  
@@ -59,12 +73,12 @@ document.addEventListener('click', function(event) {
 const defaultcolor = line.style.background;
 line.onmouseover = function displayPoint() {
     line.style.background = "#8fD362";
-    placepoint.classList.remove('isHidden');
+    hoverPoint.classList.remove('isHidden');
 }
 
 line.onmouseleave = function onMouseLeave() {
     if(clickedOnLine == false) {
-        placepoint.classList.add('isHidden');
+        hoverPoint.classList.add('isHidden');
     }
     line.style.background = defaultcolor;
 }
@@ -75,8 +89,8 @@ function cancelData() {
 
 function inactiveStylingActivate() {
     point_data.classList.add('isHidden');
-    placepoint.classList.add('isHidden');
-    placepoint.classList.remove('placepoint-active');
+    hoverPoint.classList.add('isHidden');
+    hoverPoint.classList.remove('hoverPoint-active');
 
     clickedOnLine = false;
 
@@ -85,24 +99,20 @@ function inactiveStylingActivate() {
     description_data.value = "";
 }
 
-// FIX: Rework uniqueid. Currently restarts back to 1 after importing from localStorage. Make it random x-length string instead of increasing by 1?
-var dataStore = JSON.parse(localStorage.getItem('dataLocalStorage')) || []; // imports from local storage, otherwise creates empty array
-var uniqueid = 1;
+const dataStore = JSON.parse(localStorage.getItem('dataLocalStorage')) || []; // imports from local storage, otherwise creates empty array
+let objPointData = {}
 function SubmitData() {
-    var obj = {
-        id: uniqueid,
-        title: title_data.value,
-        description: description_data.value
-    }
-    dataStore.push(obj); // stores into array
+    objPointData.id = dataStore.length;
+    objPointData.title = title_data.value;
+    objPointData.description = description_data.value;
+    dataStore.push(objPointData); // stores into array
     localStorage.setItem('dataLocalStorage', JSON.stringify(dataStore)); // serializes the array and stores in local storage
-    uniqueid++;
 
     inactiveStylingActivate();
 
     // console.log(localStorage); // for logging purposes
     // console.log(JSON.stringify(dataStore)); // for logging purposes
-    console.log('Point data stored: ' + JSON.stringify(obj)); // for logging purposes
+    console.log('Point data stored: ' + JSON.stringify(objPointData)); // for logging purposes
 }
 
 submit_button.onclick = SubmitData;
