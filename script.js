785d418273cf09f478209f88796ffa8c52c8991c
chris <thunderlight465@gmail.com>
2024-08-29 07:29:02 -0700

    Major restyling to entire page and new features - More & smoother animations - Points now are equally spaced apart - Fixed intializePointData not calculating 'point_dataMaxAllowablePosition' when resizing window. - Plotted points now show data - Added landing page. Loading the site when localStorage has 0 data will take you there.

--- script.js
+++ script.js
@@ -8,7 +8,22 @@ const submit_button = document.getElementById("submit_button");
 const title_data = document.getElementById("title_data");
 const description_data = document.getElementById("description_data");
 const mouseXYposition = document.getElementById("mouseXYposition");
-const placedPoint = document.getElementById("placedPoint");
+const dataConnectionLine = document.getElementById("dataConnectionLine");
+const hoverPointInnerCircle = document.getElementById("hoverPointInnerCircle");
+const flex_horizontal_points = document.getElementById("flex_horizontal_points");
+const placedPointDisplay = document.getElementById("placedPointDisplay");
+const pointTitle = document.getElementById("pointTitle");
+const pointDate = document.getElementById("pointDate");
+const landingPage = document.getElementById("landingPage");
+const landingCircle = document.getElementById("landingCircle");
+
+const dataStore = JSON.parse(localStorage.getItem('dataLocalStorage')) || []; // imports from local storage, otherwise creates empty array
+
+if (dataStore.length > 0) {
+    console.log('dataStore has more than 0')
+    line_container.classList.remove('isHidden')
+    landingPage.classList.add('isHidden')
+}
 
 let mousex;
 let mousey;
@@ -24,6 +39,7 @@ document.addEventListener('mousemove', function getMousePosition(event) {
 
 let saveTransitionBehavior;
 let point_dataMaxAllowablePosition
+window.addEventListener('resize', initializePointData)
 initializePointData();
 function initializePointData() { // Temporarily exposes point_data to store offsetWidth
     console.log('Initialized point_data');
@@ -32,6 +48,12 @@ function initializePointData() { // Temporarily exposes point_data to store offs
     point_data.classList.remove('isHidden');
     point_dataMaxAllowablePosition = line.offsetWidth + line.offsetLeft - point_data.offsetWidth;
     point_data.classList.add('isHidden');
+}
+
+landingCircle.onclick = function enterHorizon() {
+    landingPage.classList.add('isHidden');
+    line_container.classList.remove('isHidden');
+    initializePointData();
 }
 
  clickedOnLine = false;
@@ -44,25 +66,28 @@ line.onclick = function showPointData() {
         point_data.style.left = point_dataMaxAllowablePosition + "px";
     }
     point_data.classList.remove('isHidden');
-    hoverPoint.classList.add('hoverPoint-active');
+    dataConnectionLine.classList.remove('isHidden');
+    hoverPoint.classList.add('hoverPoint-onClick');
     hoverPoint.style.left = mousex + 'px';
     title_data.focus(); // auto focuses to title input field
     console.log("Clicked on timeline."); // Log to console
 }
 
 function placePointOnLine() {
-    placedPoint.classList.remove('isHidden');
-    placedPoint.style.left = hoverPoint.style.left;
-    point_data.classList.add('isHidden');
-    /* placedPoint.addEventListener('mouseover', function() {
-        placedPoint.style.backgroundColor = 'red';
-        point_data.classList.remove('isHidden');
-    }); */
-    line_container.appendChild(placedPoint.cloneNode(true));
-    line_container.appendChild(point_data.cloneNode(true));
+    pointTitle.innerText = title_data.value;
+    pointDate.innerText = description_data.value;
+
+    placedPointDisplay.classList.remove('isHidden');
+    hoverPoint.classList.remove('hoverPoint-onClick')
+    hoverPoint.classList.remove('isHidden');
+    hoverPoint.classList.add('hoverPoint-onPlace');
+    flex_horizontal_points.appendChild(hoverPoint.cloneNode(true));
+    placedPointDisplay.classList.add('isHidden');
+    hoverPoint.classList.add('isHidden');
+    hoverPoint.classList.remove('hoverPoint-onPlace');
 }
 
-document.addEventListener('click', function(event) {  
+document.addEventListener('click', function unfocusElement(event) {  
     if (!line.contains(event.target) && !point_data.contains(event.target)) {
         inactiveStylingActivate();
         console.log('Point unfocused!'); // Log to console
@@ -71,25 +96,31 @@ document.addEventListener('click', function(event) {
 
 const defaultcolor = line.style.background;
 line.onmouseover = function displayPoint() {
-    line.style.background = "#8fD362";
     hoverPoint.classList.remove('isHidden');
 }
 
-line.onmouseleave = function onMouseLeave() {
+/* document.addEventListener('mousemove', function onMouseLeave(event) {
+    if(clickedOnLine == false && !line.contains(event.target) && !hoverPoint.contains(event.target)) {
+        hoverPoint.classList.add('isHidden');
+        console.log('uhh');
+    }
+}) */
+line.onmouseleave = function onMouseLeave(event) {
     if(clickedOnLine == false) {
         hoverPoint.classList.add('isHidden');
     }
-    line.style.background = defaultcolor;
 }
 
 function cancelData() {
     inactiveStylingActivate();
 }
 
+window.addEventListener('resize', inactiveStylingActivate)
 function inactiveStylingActivate() {
     point_data.classList.add('isHidden');
+    dataConnectionLine.classList.add('isHidden');
+    hoverPoint.classList.remove('hoverPoint-onClick');
     hoverPoint.classList.add('isHidden');
-    hoverPoint.classList.remove('hoverPoint-active');
 
     clickedOnLine = false;
 
@@ -98,16 +129,15 @@ function inactiveStylingActivate() {
     description_data.value = "";
 }
 
-const dataStore = JSON.parse(localStorage.getItem('dataLocalStorage')) || []; // imports from local storage, otherwise creates empty array
 function SubmitData() {
-    placePointOnLine();
     let objPointData = {}
     objPointData.id = dataStore.length;
     objPointData.title = title_data.value;
     objPointData.description = description_data.value;
     dataStore.push(objPointData); // stores into array
     localStorage.setItem('dataLocalStorage', JSON.stringify(dataStore)); // serializes the array and stores in local storage
-
+    
+    placePointOnLine();
     inactiveStylingActivate();
 
     // console.log(localStorage); // for logging purposes
