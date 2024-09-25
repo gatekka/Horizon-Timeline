024440b306a9cc0ef40aee271ccfca2048262f11
chris <94351434+gatekka@users.noreply.github.com>
2024-09-25 05:08:53 -0700

    Refactor code for clarity

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 //TODO: Look into replacing arrays with object constructors? Might be more efficient.
-const line = document.getElementById("line");
-const hoverPoint = document.getElementById("hoverPoint");
-const pointSubmissionContainer = document.getElementById("pointSubmissionContainer");
-const lineContainer = document.getElementById("lineContainer");
-const submit_button = document.getElementById("submit_button");
-const titleInput = document.getElementById("titleInput");
-const description_data = document.getElementById("description_data");
-const mouseXYposition = document.getElementById("mouseXYposition");
-const dataConnectionLine = document.getElementById("dataConnectionLine");
-const hoverPointInnerCircle = document.getElementById("hoverPointInnerCircle");
-const flex_horizontal_points = document.getElementById("flex_horizontal_points");
-const placedPointDisplay = document.getElementById("placedPointDisplay");
-const pointTitle = document.getElementById("pointTitle");
-const pointDescription = document.getElementById("pointDescription");
-const landingPage = document.getElementById("landingPage");
-const landingCircle = document.getElementById("landingCircle");
-const backgroundImage = document.getElementById("backgroundImage");
+const elements = {
+    line: document.getElementById("line"),
+    hoverPoint: document.getElementById("hoverPoint"),
+    pointSubmissionContainer: document.getElementById("pointSubmissionContainer"),
+    lineContainer: document.getElementById("lineContainer"),
+    submit_button: document.getElementById("submit_button"),
+    titleInput: document.getElementById("titleInput"),
+    description_data: document.getElementById("description_data"),
+    mouseXYposition: document.getElementById("mouseXYposition"),
+    dataConnectionLine: document.getElementById("dataConnectionLine"),
+    hoverPointInnerCircle: document.getElementById("hoverPointInnerCircle"),
+    flex_horizontal_points: document.getElementById("flex_horizontal_points"),
+    placedPointDisplay: document.getElementById("placedPointDisplay"),
+    pointTitle: document.getElementById("pointTitle"),
+    pointDescription: document.getElementById("pointDescription"),
+    landingPage: document.getElementById("landingPage"),
+    landingCircle: document.getElementById("landingCircle"),
+    backgroundImage: document.getElementById("backgroundImage"),
+    dateOutputText: document.getElementById('dateOutputText'),
+    dateOutputTimecode: document.getElementById('dateOutputTimecode'),
+}
 
 let isKeyboardPlottingActive = false;
 let isEditingPoint = false;
@@ -23,9 +27,7 @@ let isEditingPoint = false;
 const dataStore = JSON.parse(localStorage.getItem('dataLocalStorage')) || []; // imports from local storage, otherwise creates empty array
 dataStore.sort((a, b) => a.timecode - b.timecode);
 
-description_data.addEventListener('input', e => parseTextToDate(e.target.value));
-const dateOutputText = document.getElementById('dateOutputText');
-const dateOutputTimecode = document.getElementById('dateOutputTimecode');
+elements.description_data.addEventListener('input', e => parseTextToDate(e.target.value));
 function parseTextToDate(input) {
     let dateString = input.trim();
     const currentYear = new Date().getFullYear();
@@ -35,8 +37,8 @@ function parseTextToDate(input) {
         dateString = `${dateString}, ${currentYear}`;
     }
     const convertToDate = new Date(dateString);
-    dateOutputText.innerHTML = convertToDate;
-    dateOutputTimecode.innerHTML = Date.parse(convertToDate);
+    elements.dateOutputText.innerHTML = convertToDate;
+    elements.dateOutputTimecode.innerHTML = Date.parse(convertToDate);
     return Date.parse(convertToDate);
 }
 
@@ -50,115 +52,21 @@ function renderPointsFromLocalStorage() {
 }
 
 //TODO: When clicking on timeline between 2 points, make it so that its somehow quicker to select date?
-submit_button.onclick = submitData;
+elements.submit_button.onclick = submitData;
 function submitData() {
-    if (titleInput.value === '' && description_data.value === '') {
+    if (elements.titleInput.value === '' && elements.description_data.value === '') {
         inactiveStylingActivate();
-    } else if (isEditingPoint) { 
-        const point = document.getElementById(`point${editingElementId}`);
-        const pointTitle = point.querySelector('.placedPointDisplay .pointTitle');
-        const pointDescription = point.querySelector('.placedPointDisplay .pointDescription');
-        pointTitle.innerText = titleInput.value;
-        pointDescription.innerText = description_data.value;
-        getArrayToEdit.title = titleInput.value;
-        getArrayToEdit.description = description_data.value;
-        getArrayToEdit.timecode = parseTextToDate(description_data.value);
-        localStorage.setItem('dataLocalStorage', JSON.stringify(dataStore));
-        inactiveStylingActivate();
-        console.log(`Confirmed edit for: ${JSON.stringify(dataStore[editingElementId])}`);
-    } else {
-        let objPointData = {
-            id: dataStore.length + 1,
-            title: titleInput.value,
-            description: description_data.value,
-            timecode: parseTextToDate(description_data.value)
-        }
-        currentID = objPointData.id;
-        dataStore.push(objPointData); // stores object into array
-        localStorage.setItem('dataLocalStorage', JSON.stringify(dataStore)); // serializes the array and stores in local storage
-        placePointOnLine(objPointData.title, objPointData.description);
-        inactiveStylingActivate();
-        console.log('Point data stored: ' + JSON.stringify(objPointData)); // for logging purposes
+        return;
     }
+    
+    if (isEditingPoint) { 
+        updatePoint();
+    } else {
+        addNewPoint();
+    }
+
     dataStore.sort((a, b) => a.timecode - b.timecode);
-    dataStore.forEach(element => {
-    const pointElement = document.getElementById(`point${element.id}`);
-    if ((pointElement.id).substring(5) != currentID) {
-        pointElement.style.animation = 'none';
-        pointElement.querySelector('.placedPointDisplay').style.animation = 'none';
-        pointElement.querySelector('.dataConnectionLine').style.animation = 'none';
-    }
-    flex_horizontal_points.appendChild(pointElement);
-    });
-}
-
-let mousex;
-let mousey;
-document.addEventListener('mousemove', function getMousePosition(event) {
-    mousex = event.pageX;
-    mousey = event.pageY;
-    const positionText= "X: " + mousex + ", Y: " + mousey;
-    mouseXYposition.innerText = positionText;
-    if (clickedOnLine === false) {
-        hoverPoint.style.left = mousex + "px"; // move point with mouse
-    }
-});
-
-let saveTransitionBehavior;
-let point_dataMaxAllowablePosition
-calculatePointDataMaxAllowablePosition();
-window.addEventListener('resize', calculatePointDataMaxAllowablePosition)
-function calculatePointDataMaxAllowablePosition() { // Temporarily exposes pointSubmissionContainer to store offsetWidth
-    console.log('Initialized pointSubmissionContainer');
-    saveTransitionBehavior = pointSubmissionContainer.style.transitionBehavior;
-    pointSubmissionContainer.style.transitionBehavior = 'initial'
-    pointSubmissionContainer.classList.remove('isHidden');
-    point_dataMaxAllowablePosition = line.offsetWidth + line.offsetLeft - pointSubmissionContainer.offsetWidth;
-    pointSubmissionContainer.classList.add('isHidden');
-}
-
-landingCircle.onclick = enterHorizon;
-function enterHorizon() {
-    console.log('Executed enterHorizon()');
-    landingPage.classList.add('isHidden');
-    lineContainer.classList.remove('isHidden');
-    document.body.classList.add('bodyTimeline');
-    backgroundImage.classList.add('backgroundImage-postEffects');
-    calculatePointDataMaxAllowablePosition();
-}
-
-function positionPointData() {
-    pointSubmissionContainer.style.transitionBehavior = saveTransitionBehavior;
-    if (mousex < point_dataMaxAllowablePosition) {
-        pointSubmissionContainer.style.left = mousex + "px";
-    } else {
-        pointSubmissionContainer.style.left = point_dataMaxAllowablePosition + "px";
-    }
-}
-
-clickedOnLine = false;
-line.addEventListener('click', showSubmissionContainer)
-function showSubmissionContainer() {
-    unfocusedPoint = false;
-    if (isEditingPoint) {
-        titleInput.value = getArrayToEdit.title;
-        description_data.value = getArrayToEdit.description;
-        positionPointData();
-    } else if (!isKeyboardPlottingActive) {
-        clickedOnLine = true;
-        clearInputs();
-        positionPointData();
-        dataConnectionLine.classList.remove('isHidden');
-        hoverPoint.classList.add('hoverPoint-onClick');
-        hoverPoint.style.left = mousex + 'px';
-        console.log("Clicked on timeline."); // Log to console
-    } else {
-        pointSubmissionContainer.style.left = 'auto';
-        console.log("Displaying pointData."); // Log to console
-        isKeyboardPlottingActive = false;
-    }
-    pointSubmissionContainer.classList.remove('isHidden');
-    setTimeout(() => {titleInput.focus()}, 1); // auto focuses to input field after 1 ms
+    renderAllPoints();
 }
 
 let editingElementId;
@@ -174,11 +82,128 @@ function editPoint(elementID) {
     showSubmissionContainer();
 }
 
-function placePointOnLine(title, description) {
-    pointTitle.innerText = title;
-    pointDescription.innerText = description;
+function updatePoint() {
+    const point = document.getElementById(`point${editingElementId}`);
+    const pointTitle = point.querySelector('.placedPointDisplay .pointTitle');
+    const pointDescription = point.querySelector('.placedPointDisplay .pointDescription');
+    pointTitle.innerText = elements.titleInput.value;
+    pointDescription.innerText = elements.description_data.value;
+    getArrayToEdit.title = elements.titleInput.value;
+    getArrayToEdit.description = elements.description_data.value;
+    getArrayToEdit.timecode = parseTextToDate(elements.description_data.value);
+    localStorage.setItem('dataLocalStorage', JSON.stringify(dataStore));
+    inactiveStylingActivate();
+    console.log(`Confirmed edit for: ${JSON.stringify(dataStore[editingElementId])}`);
+}
 
-    const clonedPoint = hoverPoint.cloneNode(true);
+function addNewPoint() {
+    let objPointData = {
+        id: dataStore.length + 1,
+        title: elements.titleInput.value,
+        description: elements.description_data.value,
+        timecode: parseTextToDate(elements.description_data.value)
+    }
+    currentID = objPointData.id;
+    dataStore.push(objPointData); // stores object into array
+    localStorage.setItem('dataLocalStorage', JSON.stringify(dataStore)); // serializes the array and stores in local storage
+    placePointOnLine(objPointData.title, objPointData.description);
+    inactiveStylingActivate();
+    console.log('Point data stored: ' + JSON.stringify(objPointData)); // for logging purposes
+}
+
+function renderAllPoints() {
+    dataStore.forEach(element => {
+        const pointElement = document.getElementById(`point${element.id}`);
+        if (element.id != currentID) {
+            // pointElement.style.animation = 'none';
+            // pointElement.querySelector('.placedPointDisplay').style.animation = 'none';
+            // pointElement.querySelector('.dataConnectionLine').style.animation = 'none';
+            disableAnimations(pointElement)
+        }
+        elements.flex_horizontal_points.appendChild(pointElement);
+    });
+}
+
+function disableAnimations(element) {
+    element.style.animation = 'none';
+    const children = element.querySelectorAll('.placedPointDisplay, .dataConnectionLine');
+    children.forEach(child => child.style.animation = 'none');
+}
+
+let mouseXPosition;
+let mouseYPosition;
+document.addEventListener('mousemove', function getMousePosition(event) {
+    mouseXPosition = event.pageX;
+    mouseYPosition = event.pageY;
+    const positionText= "X: " + mouseXPosition + ", Y: " + mouseYPosition;
+    elements.mouseXYposition.innerText = positionText;
+    if (clickedOnLine === false) {
+        elements.hoverPoint.style.left = mouseXPosition + "px"; // move point with mouse
+    }
+});
+
+let saveTransitionBehavior;
+let point_dataMaxAllowablePosition
+calculatePointDataMaxAllowablePosition();
+window.addEventListener('resize', calculatePointDataMaxAllowablePosition)
+function calculatePointDataMaxAllowablePosition() { // Temporarily exposes pointSubmissionContainer to store offsetWidth
+    console.log('Initialized pointSubmissionContainer');
+    saveTransitionBehavior = elements.pointSubmissionContainer.style.transitionBehavior;
+    elements.pointSubmissionContainer.style.transitionBehavior = 'initial'
+    elements.pointSubmissionContainer.classList.remove('isHidden');
+    point_dataMaxAllowablePosition = elements.line.offsetWidth + elements.line.offsetLeft - elements.pointSubmissionContainer.offsetWidth;
+    elements.pointSubmissionContainer.classList.add('isHidden');
+}
+
+elements.landingCircle.onclick = enterHorizon;
+function enterHorizon() {
+    console.log('Executed enterHorizon()');
+    elements.landingPage.classList.add('isHidden');
+    elements.lineContainer.classList.remove('isHidden');
+    document.body.classList.add('bodyTimeline');
+    elements.backgroundImage.classList.add('backgroundImage-postEffects');
+    calculatePointDataMaxAllowablePosition();
+}
+
+function positionPointData() {
+    elements.pointSubmissionContainer.style.transitionBehavior = saveTransitionBehavior;
+    if (mouseXPosition < point_dataMaxAllowablePosition) {
+        elements.pointSubmissionContainer.style.left = mouseXPosition + "px";
+    } else {
+        elements.pointSubmissionContainer.style.left = point_dataMaxAllowablePosition + "px";
+    }
+}
+
+clickedOnLine = false;
+elements.line.addEventListener('click', showSubmissionContainer)
+function showSubmissionContainer() {
+    unfocusedPoint = false;
+    if (isEditingPoint) {
+        elements.titleInput.value = getArrayToEdit.title;
+        elements.description_data.value = getArrayToEdit.description;
+        positionPointData();
+    } else if (!isKeyboardPlottingActive) {
+        clickedOnLine = true;
+        clearInputs();
+        positionPointData();
+        elements.dataConnectionLine.classList.remove('isHidden');
+        elements.hoverPoint.classList.add('hoverPoint-onClick');
+        elements.hoverPoint.style.left = mouseXPosition + 'px';
+        console.log("Clicked on timeline."); // Log to console
+    } else {
+        elements.pointSubmissionContainer.style.left = 'auto';
+        console.log("Displaying pointData."); // Log to console
+        isKeyboardPlottingActive = false;
+    }
+    elements.pointSubmissionContainer.classList.remove('isHidden');
+    setTimeout(() => {elements.titleInput.focus()}, 1); // auto focuses to input field after 1 ms
+}
+
+function placePointOnLine(title, description) {
+    elements.pointTitle.innerText = title;
+    elements.pointDescription.innerText = description;
+
+    const clonedPoint = elements.hoverPoint.cloneNode(true);
     clonedPoint.id = `point${currentID}`;
     clonedPoint.classList.remove('hoverPoint-onClick');
     clonedPoint.classList.remove('isHidden');
@@ -191,26 +216,26 @@ function placePointOnLine(title, description) {
             editPoint(clonedPoint.id);
         }
     });    
-    flex_horizontal_points.appendChild(clonedPoint);
+    elements.flex_horizontal_points.appendChild(clonedPoint);
 }
     
 
 let unfocusedPoint = true;
 document.addEventListener('click', function unfocusElement(event) {  
-    if (unfocusedPoint === false && !line.contains(event.target) && !pointSubmissionContainer.contains(event.target) && !flex_horizontal_points.contains(event.target)) {
+    if (unfocusedPoint === false && !elements.line.contains(event.target) && !elements.pointSubmissionContainer.contains(event.target) && !elements.flex_horizontal_points.contains(event.target)) {
         inactiveStylingActivate();
         unfocusedPoint = true;
         console.log('Point unfocused!'); // Log to console
     }
 });
 
-line.onmouseover = function displayPoint() {
-    hoverPoint.classList.remove('isHidden');
+elements.line.onmouseover = function displayPoint() {
+    elements.hoverPoint.classList.remove('isHidden');
 }
 
-line.onmouseleave = function onMouseLeave(event) {
+elements.line.onmouseleave = function onMouseLeave(event) {
     if(clickedOnLine == false) {
-        hoverPoint.classList.add('isHidden');
+        elements.hoverPoint.classList.add('isHidden');
     }
 }
 
@@ -221,18 +246,18 @@ window.addEventListener('resize', () => {
 })
 
 function clearInputs() {
-    titleInput.value = ""; 
-    description_data.value = "";
+    elements.titleInput.value = ""; 
+    elements.description_data.value = "";
 }
 
 function inactiveStylingActivate() {
-    hideElement(pointSubmissionContainer);
-    hideElement(hoverPoint);
-    pointSubmissionContainer.classList.add('isHidden');
-    hoverPoint.classList.remove('hoverPoint-onClick');
-    hoverPoint.classList.add('isHidden');
-    dataConnectionLine.classList.add('isHidden');
-    dataConnectionLine.style.animation = null;
+    hideElement(elements.pointSubmissionContainer);
+    hideElement(elements.hoverPoint);
+    elements.pointSubmissionContainer.classList.add('isHidden');
+    elements.hoverPoint.classList.remove('hoverPoint-onClick');
+    elements.hoverPoint.classList.add('isHidden');
+    elements.dataConnectionLine.classList.add('isHidden');
+    elements.dataConnectionLine.style.animation = null;
 
     clickedOnLine = false;
     isEditingPoint = false;
@@ -256,8 +281,10 @@ function hideElement(element) {
     }
 }
 
-titleInput.addEventListener('keydown', handleInputBoxesSubmit)
-description_data.addEventListener('keydown', handleInputBoxesSubmit)
+[elements.titleInput, elements.description_data].forEach(element => {
+    element.addEventListener('keydown', handleInputBoxesSubmit);
+});
+
 function handleInputBoxesSubmit(event) {
     switch (event.key) {
         case 'Enter':
@@ -274,7 +301,7 @@ function handleInputBoxesSubmit(event) {
 
 //TODO: Implement case that toggles delete mode? Or maybe only when holding down key is better.
 window.addEventListener('keydown', (event) => {
-    if (!(getComputedStyle(pointSubmissionContainer).display == 'flex')) {
+    if (!(getComputedStyle(elements.pointSubmissionContainer).display == 'flex')) {
         switch (event.key) {
             case 'Tab':
                 console.log(`Pressed ${event.key}.`);
@@ -291,7 +318,7 @@ window.addEventListener('keydown', (event) => {
     }
 })
 
-mouseXYposition.onclick = () => {
+elements.mouseXYposition.onclick = () => {
     alert(JSON.stringify(dataStore))
     localStorage.clear();
     location.reload();
